45bc0ba05efb6e1c7e8b45a423b38ad77947c6c7
Deepak Singh <2397404@AFRICA.AIRTEL.ITM>
2020-02-28 11:05:21 +0530

    changes

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 module.exports.area = radius => Math.PI*radius*radius ;
 module.exports.perimeter1 = radius => 2*Math.PI*radius ;
 module.exports.perimeter = radius => 2*Math.PI*radius ;
+module.exports.perimeter2 = radius => 2*Math.PI*radius ;
